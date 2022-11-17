1bfecf5d9f0b9445b1f5b1c2aed165839c20b93e
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-11-17 12:11:45 +0700

    initial commit

---
+++ featureextractionform.pas
@@ -0,0 +1,26 @@
+unit featureextractionform;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
